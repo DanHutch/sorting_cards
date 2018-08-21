cdc95bc23b1a6863bbf3e400b93e31f0078072b5
DanHutch <danhutch5280@gmail.com>
2018-08-21 16:30:23 -0600

    reworked some code in my sort method for readability

--- lib/deck.rb
+++ lib/deck.rb
@@ -13,20 +13,19 @@ class Deck
   end
 
   def sort
-    deck_length = count
+    swapped = true
 
-    loop do
+    until not swapped do
       swapped = false
-
-      (deck_length - 1).times do |index|
+      (count - 1).times do |index|
         if @cards[index].card_id > @cards[index + 1].card_id
           @cards[index], @cards[index + 1] = @cards[index + 1], @cards[index]
           swapped = true
         end
       end
-      break if not swapped
     end
     @cards
   end
+
 
 end
