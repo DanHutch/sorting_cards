1f8a2763991781fcf53d4ab53bc6c843a66ef6c7
DanHutch <danhutch5280@gmail.com>
2018-08-21 15:08:23 -0600

    built card value and suit ID hashes

--- lib/card.rb
+++ lib/card.rb
@@ -10,4 +10,16 @@ class Card
     @suit   = suit.capitalize
   end
 
+  def set_card_value_id
+    card_value_id = {
+      "2" => 2, "3" => 6, "4" => 10, "5" => 14, "6" => 18, "7" => 22, "8" => 26, "9" => 30, "10" => 34, "Jack" => 38, "Queen" => 42, "King" => 46, "Ace" => 50
+    }
+  end
+
+  def set_card_suit_id
+    card_suit_id = {
+      "Clubs" => 0, "Diamonds" => 1, "Hearts" => 2, "Spades" => 3
+    }
+  end
+
 end
--- lib/deck.rb
+++ lib/deck.rb
@@ -12,4 +12,21 @@ class Deck
     @cards.count
   end
 
+  def sort(sort_arg)
+    deck_length = count
+
+    loop do
+      swapped = false
+
+      (deck_length - 1).times do |index|
+        if sort_arg[index] > sort_arg[index + 1]
+          sort_arg[index], sort_arg[index + 1] = sort_arg[index + 1], array[index]
+          swapped = true
+        end
+      end
+      break if not swapped
+    end
+    sort_arg
+  end
+
 end
